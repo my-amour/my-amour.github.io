b54dbb5c876194d7f74970edfb90435463525fe2
Eduard Klimenko <edik.klimenko@gmail.com>
2022-10-27 03:26:03 +0300

    0.0.1

---
+++ 4376.e879bdf0cc668f3d.js
@@ -0,0 +1,7 @@
+"use strict";(self.webpackChunkapp=self.webpackChunkapp||[]).push([[4376],{4376:(E,u,r)=>{r.r(u),r.d(u,{ion_text:()=>l});var e=r(1308),c=r(9658),_=r(2854);
+/*!
+     * (C) Ionic http://ionicframework.com - MIT License
+     */const l=class{constructor(s){(0,e.r)(this,s)}render(){const s=(0,c.b)(this);return(0,e.h)(e.H,{class:(0,_.c)(this.color,{[s]:!0})},(0,e.h)("slot",null))}};l.style=":host(.ion-color){color:var(--ion-color-base)}"},2854:(E,u,r)=>{r.d(u,{c:()=>_,g:()=>l,h:()=>c,o:()=>d});var e=r(5861);
+/*!
+     * (C) Ionic http://ionicframework.com - MIT License
+     */const c=(t,n)=>null!==n.closest(t),_=(t,n)=>"string"==typeof t&&t.length>0?Object.assign({"ion-color":!0,[`ion-color-${t}`]:!0},n):n,l=t=>{const n={};return(t=>void 0!==t?(Array.isArray(t)?t:t.split(" ")).filter(o=>null!=o).map(o=>o.trim()).filter(o=>""!==o):[])(t).forEach(o=>n[o]=!0),n},s=/^[a-z][a-z0-9+\-.]*:/,d=function(){var t=(0,e.Z)(function*(n,o,f,h){if(null!=n&&"#"!==n[0]&&!s.test(n)){const i=document.querySelector("ion-router");if(i)return o?.preventDefault(),i.push(n,f,h)}return!1});return function(o,f,h,i){return t.apply(this,arguments)}}()}}]);
